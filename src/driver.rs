8fc05875811d85cd666b0f72dadd8a20648964c3
Jonathan D. Baker <jonathan@ardent.tech>
2025-07-25 20:11:22 -0600

    roughly impl more registers for page 0 and 1

--- src/driver.rs
+++ src/driver.rs
@@ -4,6 +4,8 @@ use crate::error::TLV320DAC3100Error;
 
 const I2C_DEVICE_ADDRESS: u8 = 0x18;
 const PAGE_CONTROL_REGISTER: u8 = 0x00;
+
+// page 0 registers
 const SOFTWARE_RESET: u8 = 0x01;
 const OT_FLAG: u8 = 0x03;
 const CLOCK_GEN_MUXING: u8 = 0x04;
@@ -11,7 +13,20 @@ const PLL_P_AND_R_VALUES: u8 = 0x05;
 const PLL_J_VALUE: u8 = 0x06;
 const PLL_D_VALUE_MSB: u8 = 0x07;
 const PLL_D_VALUE_LSB: u8 = 0x08;
+const DAC_NDAC_VAL: u8 = 0x0b;
+const DAC_DOSR_VAL_MSB: u8 = 0x0d;
+const DAC_DOSR_VAL_LSB: u8 = 0x0e;
+//const CODEC_SECONDARY_INTERFACE_CONTROL_1: u8 = 0x1f;
+const DAC_DATA_PATH_SETUP: u8 = 0x3f;
+const DAC_VOLUME_CONTROL: u8 = 0x40;
+const DAC_LEFT_VOLUME_CONTROL: u8 = 0x41;
+const DAC_RIGHT_VOLUME_CONTROL: u8 = 0x42;
 const VOL_MICDET_PIN_GAIN: u8 = 0x75;
+
+// page 1 registers
+const HEADPHONE_DRIVERS: u8 = 0x1f;
+const DAC_L_AND_DAC_R_OUTPUT_MIXER_ROUTING: u8 = 0x23;
+const HPL_DRIVER: u8 = 0x28;
 
 #[derive(Debug, PartialEq)]
 enum PllClkin {
@@ -53,6 +68,44 @@ impl TryFrom<u8> for CodecClkin {
     }
 }
 
+#[derive(Debug, PartialEq)]
+enum HeadphoneCommon {
+    C135V = 0x00,
+    C15V = 0x01,
+    C165V = 0x02,
+    C18V = 0x03,
+}
+
+#[derive(Debug, PartialEq)]
+enum LeftDataPath {
+    Off = 0x0,
+    Left = 0x1,
+    Right = 0x2,
+    Both = 0x3
+}
+
+#[derive(Debug, PartialEq)]
+enum RightDataPath {
+    Off = 0x0,
+    Right = 0x1,
+    Left = 0x2,
+    Both = 0x3
+}
+
+#[derive(Debug, PartialEq)]
+enum VolumeControl {
+    IndependentChannels = 0x0,
+    LeftToRight = 0x1,
+    RightToLeft = 0x2,
+}
+
+#[derive(Debug, PartialEq)]
+enum VolumeControlSoftStepping {
+    OneStepPerPeriod = 0x0,
+    OneStepPerTwoPeriods = 0x1,
+    Disabled = 0x2
+}
+
 pub struct TLV320DAC3100<I2C, D> {
     delay: D,
     i2c: I2C,
@@ -64,6 +117,13 @@ type TLV320Result<I2C: I2c> = Result<(), TLV320DAC3100Error<I2C::Error>>;
 impl<I2C: I2c, D: hal::delay::DelayNs> TLV320DAC3100<I2C, D> {
     pub fn new(delay: D, i2c: I2C) -> Self {
         TLV320DAC3100 { delay, i2c }
+    }
+
+    pub fn get_clock_gen_muxing(&mut self, pll_clkin: &mut PllClkin, codec_clkin: &mut CodecClkin) -> TLV320Result<I2C> {
+        let reg_val = self.read_reg(0, CLOCK_GEN_MUXING)?;
+        *pll_clkin = ((reg_val & 0xc) >> 2).try_into().unwrap();
+        *codec_clkin = (reg_val & 0x3).try_into().unwrap();
+        Ok(())
     }
 
     pub fn get_ot_flag(&mut self, ot: &mut bool) -> TLV320Result<I2C> {
@@ -107,17 +167,74 @@ impl<I2C: I2c, D: hal::delay::DelayNs> TLV320DAC3100<I2C, D> {
         Ok(self.i2c.write(I2C_DEVICE_ADDRESS, &[PAGE_CONTROL_REGISTER, page]).map_err(TLV320DAC3100Error::I2C)?)
     }
 
-    pub fn get_clock_gen_muxing(&mut self, pll_clkin: &mut PllClkin, codec_clkin: &mut CodecClkin) -> TLV320Result<I2C> {
-        let reg_val = self.read_reg(0, CLOCK_GEN_MUXING)?;
-        *pll_clkin = ((reg_val & 0xc) >> 2).try_into().unwrap();
-        *codec_clkin = (reg_val & 0x3).try_into().unwrap();
-        Ok(())
-    }
-
     pub fn set_clock_gen_muxing(&mut self, pll_clkin: PllClkin, codec_clkin: CodecClkin) -> TLV320Result<I2C> {
         let mut reg_val = (pll_clkin as u8) << 2;
         reg_val |= codec_clkin as u8;
         self.write_reg(0, CLOCK_GEN_MUXING, reg_val)
+    }
+
+    // pub fn set_codec_secondary_interface_control_1(&mut self, bclk_from_gpio1: bool, wclk_from_gpio1: bool, din_from_gpio1: bool) -> TLV320Result<I2C> {
+    //     let mut reg_val: u8 = (bclk_from_gpio1 as u8) << 5;
+    //     reg_val |= (wclk_from_gpio1 as u8) << 2;
+    //     reg_val |= din_from_gpio1 as u8;
+    //     self.write_reg(0, CODEC_SECONDARY_INTERFACE_CONTROL_1, reg_val)
+    // }
+
+    pub fn set_dac_data_path_setup(
+        &mut self,
+        left_powered: bool,
+        right_powered: bool,
+        left_data_path: LeftDataPath,
+        right_data_path: RightDataPath,
+        soft_stepping: VolumeControlSoftStepping
+    ) -> TLV320Result<I2C> {
+        let mut reg_val: u8 = (left_powered as u8) << 7;
+        reg_val |= (right_powered as u8) << 6;
+        reg_val |= (left_data_path as u8) << 4;
+        reg_val |= (right_data_path as u8) << 2;
+        reg_val |= soft_stepping as u8;
+        self.write_reg(0, DAC_DATA_PATH_SETUP, reg_val)
+    }
+
+    pub fn set_dac_dosr_val(&mut self, osr: u16) -> TLV320Result<I2C> {
+        // TODO validate
+        let msb = (osr >> 14) as u8;
+        let lsb: u8 = (0x00ff & osr) as u8;
+        self.write_reg(0, DAC_DOSR_VAL_MSB, msb)?;
+        self.write_reg(0, DAC_DOSR_VAL_LSB, lsb)
+    }
+
+    pub fn set_dac_ndac_val(&mut self, divider: u8) -> TLV320Result<I2C> {
+        if divider == 0 || divider > 128 { return Err(TLV320DAC3100Error::InvalidArgument) }
+        self.write_reg(0, DAC_NDAC_VAL, divider)
+    }
+
+    // TODO unit test
+    pub fn set_dac_volume_control(
+        &mut self,
+        left_muted: bool,
+        right_muted: bool,
+        control: VolumeControl
+    ) -> TLV320Result<I2C> {
+        let mut reg_val = (left_muted as u8) << 3;
+        reg_val |= (right_muted as u8) << 2;
+        reg_val |= control as u8;
+        self.write_reg(0, DAC_VOLUME_CONTROL, reg_val)
+    }
+
+    // TODO unit test
+    pub fn set_headphone_drivers(
+        &mut self,
+        left_powered: bool,
+        right_powered: bool,
+        common: HeadphoneCommon,
+        power_down_on_scd: bool
+    ) -> TLV320Result<I2C> {
+        let mut reg_val = (left_powered as u8) << 7;
+        reg_val |= (right_powered as u8) << 6;
+        reg_val |= (common as u8) << 3;
+        reg_val |= (power_down_on_scd as u8) << 1;
+        self.write_reg(1, HEADPHONE_DRIVERS, reg_val)
     }
 
     pub fn set_pll_d_value(&mut self, d: u16) -> TLV320Result<I2C> {
